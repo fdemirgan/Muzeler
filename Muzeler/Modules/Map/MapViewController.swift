7c430f9f6f286725ab8a50c285875db89d37dc57
Ferhat_Demirgan <fdemirgann@gmail.com>
2024-03-22 17:21:52 +0300

    added Storyboard mapKitView

---
+++ Muzeler/Modules/Map/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  Muzeler
+//
+//  Created by Ferhat on 22.03.2024.
+//
+
+import UIKit
+
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+}
