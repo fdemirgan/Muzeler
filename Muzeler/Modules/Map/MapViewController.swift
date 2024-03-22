2de7b40618340c1cdcc9c390a9e2df653e536678
Ferhat_Demirgan <fdemirgann@gmail.com>
2024-03-22 17:39:00 +0300

    added User Location point

--- Muzeler/Modules/Map/MapViewController.swift
+++ Muzeler/Modules/Map/MapViewController.swift
@@ -6,16 +6,33 @@
 //
 
 import UIKit
-
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        LocationManager.shared.locationDelegate = self
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+    }
+}
 
-        
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension MapViewController: LocationManagerDelegate {
+    func didUpdateLocation(latitude: Double, longitude: Double) {
+        print( "Kullanıcı anlık konumu: \(latitude) - \(longitude)")
     }
     
-
     
 }
